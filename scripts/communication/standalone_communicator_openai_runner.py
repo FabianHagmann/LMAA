ae24d8cd8aa489517ca9a1f0895101bf72223862
Fabian Hagmann <e12021352@student.tuwien.ac.at>
2023-03-23 20:36:27 +0100

    [MAN6] Updated standalone runner

--- scripts/communication/standalone_communicator_openai_runner.py
+++ scripts/communication/standalone_communicator_openai_runner.py
@@ -1,21 +1,44 @@
-from scripts.communication.impl.communicator_openai_completion_OUTDATED import OpenAICommunicatorImpl
+import argparse
+
+from openai.cli import bcolors
+
+from scripts.communication.impl.communicator_openai_chat_completion import OpenAICommunicatorImpl
+
+# parse arguments
+parser = argparse.ArgumentParser(
+    prog='OpenAI Chat Standalone Runner',
+    description='Script to automatically call OpenAI Chat-Completion')
+parser.add_argument('-m', '--model',
+                    action='store',
+                    choices=['gpt-3.5-turbo'],
+                    help='OpenAI model to be called - default gpt-3.5-turbo',
+                    required=False,
+                    default='gpt-3.5-turbo')
+parser.add_argument('-p', '--prompt',
+                    action='store',
+                    help='Prompt to be sent the the selected OpenAI model',
+                    required=True)
+args = parser.parse_args()
+
+# fetch values from parser
+model = args.model
+prompt = args.prompt
+request_parameters = {"prompt": prompt, 'model': model}
 
 # instantiate communicator
 com = OpenAICommunicatorImpl()
 
-# define request parameters
-request_prompt = "Implement a java method 'void sum(int[] numbers)' that calculates the sum of all elements in the " \
-                 "array."
-request_model = "code-davinci-002"
-request_parameters = {"prompt": request_prompt, 'model': request_model, 'max_tokens': 256}
-
 # validate request parameters
 valid = com.validate_request_parameters(request_parameters)
-print('Request parameters are ' + ('valid' if valid else 'invalid'))
+if not valid:
+    print(f"{bcolors.WARNING}Error: requested parameters are not valid{bcolors.ENDC}")
+    exit(1)
 
 # send request
 solution = com.send_request(request_parameters)
+if solution != 200:
+    print(f"{bcolors.WARNING}Error: response code {solution.code} \n {solution.payload} {bcolors.ENDC}")
+    exit(1)
+
 print("----------------------------------------\nGenerated Solution\n----------------------------------------")
-print(solution)
-
-
+print(solution.payload)
