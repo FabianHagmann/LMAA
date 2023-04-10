78e992b3632ddd1dae980c94d1df52b63d5f6d31
Fabian Hagmann <hagmannfabian@gmail.com>
2023-04-10 19:57:09 +0200

    [VIS1] add functionality to view testresults for solutions

--- gui/visualization/views.py
+++ gui/visualization/views.py
@@ -1,7 +1,9 @@
+from django.db.models.functions import datetime
 from django.http import JsonResponse
 from django.views.generic import TemplateView
 
 from gui.assignments.models import Assignment, Solution
+from gui.testing.models import Testresult, ContainsTestcase, CompilesTestcase, UnitTestcase
 
 
 class VisualizationOverview(TemplateView):
@@ -30,7 +32,72 @@ class VisualizeSingleSolution(TemplateView):
         solution = Solution.objects.get(id=solution_id)
         context['solution'] = solution
 
+        context['containsTestcases'] = ContainsTestcase.objects.filter(assignment_id=solution.assignment_id) \
+            .order_by('phrase').all()
+        context['test_results'] = self.__build_existing_test_results__(solution_id)
+
         return context
+
+    def __build_existing_test_results__(self, solution_id: int) -> dict[
+        datetime.datetime, dict[str, Testresult | dict[ContainsTestcase, Testresult]]]:
+        """
+            {
+                timestamp_1: {
+                    'compiles': Testresult,
+                    'contains': {
+                        ContainsTestcase1: Testresult,
+                        ContainsTestcase2: Testresult,
+                    }
+                    'unit': Testresult
+                },
+                ...
+            }
+        """
+
+        result = {}
+
+        # get all executed timestamps for the assignment
+        timestamps = Testresult.objects.filter(solution_id=solution_id) \
+            .order_by('timestamp') \
+            .values_list('timestamp', flat=True) \
+            .distinct()
+        assignment = Solution.objects.get(id=solution_id).assignment
+
+        for ts in timestamps:
+            result[ts] = {}
+
+            compiles_test_cases = CompilesTestcase.objects.filter(assignment=assignment)
+            compiles_test_results = Testresult.objects.filter(testcase__assignment_id=assignment.id,
+                                                              solution_id=solution_id,
+                                                              testcase__in=compiles_test_cases,
+                                                              timestamp=ts)
+
+            if compiles_test_results.exists():
+                result[ts]['compiles'] = compiles_test_results.first()
+
+            contains_test_cases = ContainsTestcase.objects.filter(assignment=assignment)
+            contains_test_results = Testresult.objects.filter(testcase__assignment_id=assignment.id,
+                                                              solution_id=solution_id,
+                                                              testcase__in=contains_test_cases,
+                                                              timestamp=ts)
+
+            if contains_test_results.exists():
+                result[ts]['contains'] = {}
+                for ctr in contains_test_results:
+                    ctc = ContainsTestcase.objects.get(id=ctr.testcase_id)
+                    if ctc not in result[ts]['contains']:
+                        result[ts]['contains'][ctc] = ctr
+
+            unit_test_cases = UnitTestcase.objects.filter(assignment=assignment)
+            unit_test_results = Testresult.objects.filter(testcase__assignment_id=assignment.id,
+                                                          solution_id=solution_id,
+                                                          testcase__in=unit_test_cases,
+                                                          timestamp=ts)
+
+            if unit_test_results.exists():
+                result[ts]['unit'] = unit_test_results.first()
+
+        return result
 
 
 class SolutionWrapper():
